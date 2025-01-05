93a7a58df9185d340327761af07ac0d1dd1a0a7e
abdullahjim29 <abdullahjim2002@gmail.com>
2025-01-05 16:57:30 +0600

    project initialized and navbar created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header/Header'
+function App() {
+  return (
+    <>
+      {/* header */}
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,93 @@
+const Header = () => {
+  return (
+    <div className="w-11/12 mx-auto">
+      <nav>
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-xl"
+              >
+                <li>
+                  <a>Home</a>
+                </li>
+                <li>
+                  <a>Recipes</a>
+                </li>
+                <li>
+                  <a>About</a>
+                </li>
+                <li>
+                  <a>Search</a>
+                </li>
+              </ul>
+            </div>
+            <a href="#" className="text-4xl font-medium">RJ-Cafe</a>
+          </div>
+
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 text-xl">
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Recipes</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Search</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <div className="flex items-center gap-3">
+            <label className="input input-bordered flex items-center gap-2 rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input type="text" className="grow" placeholder="Search" />
+            </label>
+            <div className="bg-green-400 py-2 px-3 rounded-full">
+            <i class="fa-solid fa-user"></i>
+            </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
